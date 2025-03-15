3f369fee2751d922cf6280955727dc2d4af0f24e
BraulioCespedes <braulio@fullstacklabs.co>
2025-03-14 21:22:27 -0400

    First working version of the API gateway.

---
+++ src/main/java/com/app/stargateapigateway/StargateApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.app.stargateapigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StargateApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StargateApiGatewayApplication.class, args);
+	}
+
+}
